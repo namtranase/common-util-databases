184ef8f441a32837479cde5b132b16db62484c5d
tranducnamASE <trannam.ase@gmail.com>
2020-10-07 22:11:50 +0700

    update: create test_mysql program

---
+++ tests/test_mysql.py
@@ -0,0 +1,19 @@
+from src import mysql
+
+def create_database():
+    pass
+
+def insert_data():
+    pass
+
+def remove_data():
+    pass
+
+def main():
+    mysql = mysql.DB(config)
+    create_database()
+    insert_data()
+    remove_data()
+
+if __name__ == "__main__":
+    main()
