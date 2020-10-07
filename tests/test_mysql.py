7beeef912c8c611845c313501777936374c3b188
tranducnamASE <trannam.ase@gmail.com>
2020-10-07 22:22:51 +0700

    update: mysql test program

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -4,20 +4,26 @@ import settings
 from src.config.config import read_config_file
 from src import mysql
 
-def create_database():
+def create_database(mysql):
     pass
 
-def insert_data():
+def insert_data(mysql):
     pass
 
-def remove_data():
+def remove_data(mysql):
     pass
 
 def main():
+    config = read_config_file(settings.config_file)
+    if config['debug']:
+        logging.basicConfig(level=logging.DEBUG)
+
+    logging.debug('Start test mysql application with config: %s', config)
+    
     mysql = mysql.DB(config)
-    create_database()
-    insert_data()
-    remove_data()
+    create_database(mysql)
+    insert_data(mysql)
+    remove_data(mysql)
 
 if __name__ == "__main__":
     main()
